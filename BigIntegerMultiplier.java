cb9130bca4bd1952c4db738b11c43e2b4b564a81
Khugshal, Gaurabh <gaurabh.khugshal@kronos.com>
2019-10-20 20:26:25 +0530

    line 75 as well

--- BigIntegerMultiplier.java
+++ BigIntegerMultiplier.java
@@ -82,7 +82,7 @@ public class BigIntegerMultiplier {
 		for(int i = 0;i<appendZeros.length;i++){
 			
 			if(i % 2 == 0){
-				System.out.println("Hello , Now writing the logic of generating merge conflict");
+				System.out.println("Hello , Now writing the logic of generatingPFDSFS merge conflict");
 				String s = "India";
 				s = s + " is my country.";
 				System.out.println(s);
